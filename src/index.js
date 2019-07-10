9dcab9db7eece2f30db258239e9f665547c0220a
fisker <lionkay@gmail.com>
2019-07-10 15:32:50 +0800

    feat: supports sets

--- src/index.js
+++ src/index.js
@@ -2,34 +2,116 @@ function lengthAccumulator(accumulator, {length}) {
   return accumulator * length
 }
 
+const hasOwn = Object.prototype.hasOwnProperty
+
+function isValidSet(iterable) {
+  return getIterableLength(iterable) > 0
+}
+
+function hasLength(iterable) {
+  return hasOwn.call(iterable, 'length')
+}
+
+function breakableForEach(iterable, iteratee) {
+  if (hasLength(iterable)) {
+    const {length} = iterable
+    for (let i = 0; i < length; i += 1) {
+      const shouldContinue = iteratee(iterable[i], i, iterable)
+
+      if (shouldContinue === false) {
+        break
+      }
+    }
+
+    return
+  }
+
+
+  const iterator = iterable[Symbol.iterator]()
+
+  let index = 0
+  let {value, done} = iterator.next()
+  while(!done) {
+    const shouldContinue = iteratee(value, index, iterable)
+
+    if (shouldContinue === false) {
+      break
+    }
+
+    ;({value, done} = iterator.next())
+    index ++
+  }
+}
+
+function getIterableElementByIndex(iterable, index) {
+  if (hasLength(iterable)) {
+    return iterable[index]
+  }
+
+  let element
+
+  breakableForEach(iterable, (value, currentIndex) => {
+    if (index === currentIndex) {
+      element = value
+      return false
+    }
+  })
+
+  return element
+}
+
+function getIterableLength(iterable) {
+  if (hasLength(iterable)) {
+    return iterable.length
+  }
+
+  // Set#size
+  if ('size' in iterable) {
+    return iterable.size
+  }
+
+  let length = 0
+
+  breakableForEach(iterable, () => length += 1)
+
+  return length
+}
+
 class FastCartesianProduct {
   constructor(sets) {
+    if (!isValidSet(sets)) {
+      throw new Error('sets should not be empty')
+    }
+
+    breakableForEach(sets, (subsets) => {
+      if (!isValidSet(subsets)) {
+        throw new Error('sets should not have empty element')
+      }
+    })
+
     this.sets = sets
   }
 
   get(index) {
-    const setsArray = this.sets
-    const {length: setsLength} = setsArray
+    const setsLength = getIterableLength(this.sets)
     const set = new Array(setsLength)
 
     let indexRemaining = index
 
-    let j = index
-
-    for (let i = setsLength -1; i >= 0; i-=1) {
-      const subSets = this.sets[i]
-
-      const {length} = subSets
-      const index = indexRemaining % length;
+    for (let i = setsLength - 1; i >= 0; i -= 1) {
+      const subSets = getIterableElementByIndex(this.sets, i)
+      const length = getIterableLength(subSets)
+      const index = indexRemaining % length
       indexRemaining -= index
       indexRemaining /= length
-      set[i] = subSets[index]
+      set[i] = getIterableElementByIndex(subSets, index)
     }
+
 
     return set
   }
 
-  [Symbol.iterator] () {
+  [Symbol.iterator]() {
     const instance = this
     const {size} = instance
     let index = 0
@@ -43,19 +125,19 @@ class FastCartesianProduct {
 
         return {
           value,
-          done
+          done,
         }
-      }
+      },
     }
   }
 
   get size() {
     let size = 1
-    const {length: setsLength} = this.sets
-    for (let i = 0; i < setsLength; i+=1) {
-      const subSets = this.sets[i]
-      const {length} = subSets
-      size = size * length
+    const setsLength = getIterableLength(this.sets)
+    for (let i = 0; i < setsLength; i += 1) {
+      const subSets = getIterableElementByIndex(this.sets, i)
+      const length = getIterableLength(subSets)
+      size *= length
       if (!isFinite(size)) {
         break
       }
