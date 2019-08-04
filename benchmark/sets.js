fd827d584732097bf1936aaef7c5faa56606b2cd
fisker <lionkay@gmail.com>
2019-08-04 22:29:46 +0800

    ci: fix ci

--- benchmark/sets.js
+++ benchmark/sets.js
@@ -1,11 +1,19 @@
 const array = [
-  makeArray(2 ** 16, 1),
-  makeArray(1, 2 ** 16),
-  makeArray(2, 16),
+  makeArray(2 ** 8, 1),
+  makeArray(1, 2 ** 8),
   makeArray(4, 8),
-  makeArray(8, 4),
+  makeArray(8, 8),
+  makeArray(2, 16),
   makeArray(16, 2),
+  makeArray(random(2, 8), random(2, 8)),
+  makeArray(random(2, 8), random(2, 8)),
+  makeArray(random(2, 8), random(2, 8)),
+  makeArray(random(2, 8), random(2, 8)),
 ]
+
+function random(low, high) {
+  return low + Math.floor(Math.random(high - low))
+}
 
 function makeArray(width, height) {
   return {
